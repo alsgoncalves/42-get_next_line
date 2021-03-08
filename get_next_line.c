8df10d1b235d3d20b1b31d12cede979c662f94a1
Ana Goncalves <alsgoncalves91@gmail.com>
2021-03-08 13:12:25 +0000

    functional gnl

--- get_next_line.c
+++ get_next_line.c
@@ -11,60 +11,58 @@ int		save_line(char **storage, char **line)
 	if (ft_strchr(*storage, '\n'))
 	{
 		*line = ft_substr(*storage, 0, len);
-		temporary = ft_strdup(ft_strchr(*storage, '\n'));
+		temporary = ft_strdup(ft_strchr(*storage, '\n') + 1);
 		free(*storage);
 		*storage = temporary;
 		return (1);
 	}
 	else 
-		return (0);
+		return 0;
+}
+
+void		store_and_print(char **storage, char *buffer)
+{ 
+	char *temporary;
+
+	temporary = 0;
+	if (!(*storage))
+		*storage = ft_strdup(buffer);
+	else
+	{
+		temporary = *storage;
+		*storage = ft_strjoin(temporary, buffer);
+		free(temporary);
+	}
 }
 
 int get_next_line(int fd, char **line)
 {
 	static char *storage[1024];
-	char *temporary;
 	size_t bytes_read;
 	char buffer[BUFFER_SIZE + 1];
 	
-	temporary = 0;
-
 	if (read(fd, 0, 0) == -1 || !line || BUFFER_SIZE < 1 || fd < 0)
 		return (-1);
 	
-	//buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char)); 
+	if (storage[fd] && save_line(&storage[fd], line))
+		return (1);
 
 	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{	
 		buffer[bytes_read] = '\0';
-		if (!storage[fd])
-		{
-			storage[fd] = ft_strdup(buffer);
-		}
-		else
-		{
-			temporary = storage[fd];
-			storage[fd] = ft_strjoin(temporary, buffer);
-			free(temporary);
-		}
+		store_and_print(&storage[fd], buffer);
 		if (save_line(&storage[fd], line))
-		{
 			return (1);
-		}
 	}	
-
-	if ((bytes_read = read(fd, buffer, BUFFER_SIZE)) == 0 && !(storage[fd]))
-	{
-		*line = ft_strdup("");
-		return 0;
-	}
-	if (!ft_strchr(storage[fd], '\n'))
+	if (storage[fd] && !ft_strchr(storage[fd], '\n'))
 	{
 		*line = ft_strdup(storage[fd]);
 		free(storage[fd]);
 		storage[fd] = 0;
 		return (0);
 	}
+	if ((bytes_read = read(fd, buffer, BUFFER_SIZE)) == 0 && !(storage[fd]))
+		*line = ft_strdup("");
 	return 0;
 }
 
@@ -74,15 +72,12 @@ int get_next_line(int fd, char **line)
 // 	char *line;
 // 	//size_t bytes_read;
 
-// 	fd = open("file.txt" , O_RDONLY);
+// 	fd = open("gnlTester/files/alternate_line_nl_no_nl" , O_RDONLY);
 // 	if(fd == -1)
 // 	{
 // 		printf("Error opening file");
 // 		return (-1);
 // 	}
-// 	// bytes_read = read(fd, 0, "");
-// 	// printf("bytes: %i\n", bytes_read);
-	
 // 	while (get_next_line(fd, &line))
 // 	{
 // 		printf("LINE: %s\n", line);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -16,11 +16,11 @@ char	*ft_strchr(const char *s, int c)
 	while (*s != 0)
 	{
 		if (*s == (unsigned char)c)
-			return ((char *)(s + 1));
+			return ((char *)(s));
 		s++;
 	}
 	if (c == '\0' && *s == '\0')
-		return ((char *)(s + 1));
+		return ((char *)(s));
 	return (NULL);
 }
 
